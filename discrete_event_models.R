60745636e5fdfbeee3c81b5ad01371df5e02293c
awilson12 <apfeifer@email.arizona.edu>
2021-01-04 14:31:35 -0700

    commenting - left question for Mark about hand hygiene efficacy

--- discrete_event_models.R
+++ discrete_event_models.R
@@ -5,13 +5,16 @@
 #1=fomite 1 contact
 #2=fomite 2 contact
 #3=hand hygiene event
-
+#4=hand-to-face contact (dose moment)
 
 #discrete event model sequences
 
 for(a in 1:iter){
   
+  #designate timing of face contact
   facecontact<-sample(c(1:20),1)
+  
+  #these event patterns correspond to figures in current manuscript draft
   
   #symmetrical models
   discrete.1<-rep(c(1,2,1,2,3),4)
@@ -24,23 +27,30 @@ for(a in 1:iter){
   #timing of hand-to-mouth contact
   
   if(facecontact!=1 & facecontact!=20){
+    #the first position correspondes to time zero, or 0 event
+    #this is followed by the events before the timing of the hand-to-face contact
+    #followed by the events that follow the hand-to-face contact
     discrete.1<-c(0,discrete.1[1:(facecontact-1)],4,discrete.1[facecontact:20])
     discrete.2<-c(0,discrete.2[1:(facecontact-1)],4,discrete.2[facecontact:20])
     discrete.3<-c(0,discrete.3[1:(facecontact-1)],4,discrete.3[facecontact:20])
     discrete.4<-c(0,discrete.4[1:(facecontact-1)],4,discrete.4[facecontact:20])
   }else if (facecontact==1){
+    #if the hand-to-face contact happens first, we place it before the other events
     discrete.1<-c(0,4,discrete.1)
     discrete.2<-c(0,4,discrete.2)
     discrete.3<-c(0,4,discrete.3)
     discrete.4<-c(0,4,discrete.4)
     #print('yes')
   }else{
+    #if the hand-to-face contact happens last, we place it after the other events
     discrete.1<-c(0,discrete.1,4)
     discrete.2<-c(0,discrete.2,4)
     discrete.3<-c(0,discrete.3,4)
     discrete.4<-c(0,discrete.4,4)
   }
   
+  #matrix where the row corresponds to the discrete event model and the column
+  #corresponds to the event # where we track concentration changes and doses per event
   matrix<-matrix(nrow=4,ncol=length(discrete.1))
   matrix[1,]<-discrete.1
   matrix[2,]<-discrete.2
@@ -49,7 +59,10 @@ for(a in 1:iter){
   
   for (j in 1:4){
     
+    #Use the right or left hand? Randomly sampling for all events
     hand<-sample(c("R","L"),duration+1,replace=TRUE)
+    
+    #initializing vectors for storing concentrations on right and left hands, fomites, and dose over the events
     handR<-rep(0,duration+1)
     handL<-rep(0,duration+1)
     fome1<-rep(NA,duration+1)
@@ -58,6 +71,8 @@ for(a in 1:iter){
     fome2[1]<-5
     dose<-rep(NA,duration+1)
     dose[1]<-0
+    
+    #"event" is the series of events for discrete model j
     event<-matrix[j,]
     
     for (i in 2:(duration+1)){
@@ -65,17 +80,26 @@ for(a in 1:iter){
       
       if (event[i]==1){
         
+        #for a hand-to-fome 1 contact....
+        
+        #conc on fome 2 = its previous conc accounting for loss due to inactivation
         fome2[i]<-fome2[i-1]*exp(-inactiv.fome[a])
+        #we're tracking cumulative dose, so no change in dose during this event
         dose[i]<-dose[i-1]
         
         if(hand[i]=="R"){
+          #if they use the right hand...
           
+          #conc on fome 1 changed based on what is transfered from handR and what's lost due to inactivation and transfer to hand
           fome1[i]<-fome1[i-1]*exp(-inactiv.fome[a])-(S.H[a]*(A.hand[a]/SA.fome.1[a]))*(TE.SH[a]*fome1[i-1]*inactiv.fome[a]-TE.HS[a]*handR[i-1]*exp(-inactiv.hands[a]))
+          #conc on handR changed based on what is transferred from fomite 1 and what's lost due to inactivation and transfer from hand
           handR[i]<-handR[i-1]*exp(-inactiv.hands[a])-TE.HS[a]*S.H[a]*handR[i-1]*exp(-inactiv.hands[a])+TE.SH[a]*S.H[a]*fome1[i-1]*exp(-inactiv.fome[a])
           
+          #conc on left hand only changed by inactivation
           handL[i]<-handL[i-1]*exp(inactiv.hands[a])
           
         }else{
+          #if use left hand, same pattern as above but with focus on left hand as opposed to right hand
           
           fome1[i]<-fome1[i-1]*exp(-inactiv.fome[a])-(S.H[a]*(A.hand[a]/SA.fome.1[a]))*(TE.SH[a]*fome1[i-1]*inactiv.fome[a]-TE.HS[a]*handL[i-1]*exp(-inactiv.hands[a]))
           handL[i]<-handL[i-1]*exp(-inactiv.hands[a])-TE.HS[a]*S.H[a]*handL[i-1]*exp(-inactiv.hands[a])+TE.SH[a]*S.H[a]*fome1[i-1]*exp(-inactiv.fome[a])
@@ -84,6 +108,7 @@ for(a in 1:iter){
         }
         
       }else if (event[i]==2){
+        #if it's a contact with fome 2, same pattern as for fome 1
         
         fome1[i]<-fome1[i-1]*exp(-inactiv.fome[a])
         dose[i]<-dose[i-1]
@@ -105,20 +130,29 @@ for(a in 1:iter){
         }
         
       }else if (event[i]==3){
+        #if it's a hand hygiene moment...
+        
         dose[i]<-dose[i-1]
         
+        #concentration on hands changes by anticipated reduction due to hand washing
+        # !!!! Question here.. should we be accounting for reduction of concentration after accounting for inactivation on hands?
         handR[i]<-handR[i-1]/(10^handsan[a])
         handL[i]<-handL[i-1]/(10^handsan[a])
         
+        #fomite concentrations reduced by inactivation
         fome1[i]<-fome1[i-1]*exp(-inactiv.fome[a])
         fome2[i]<-fome2[i-1]*exp(-inactiv.fome[a])
       }else{
         #hand to mouth contact
+        
+        #fome concentrations only changed by inactivation
         fome1[i]<-fome1[i-1]*exp(-inactiv.fome[a])
         fome2[i]<-fome2[i-1]*exp(-inactiv.fome[a])
         
+        #hand conc and dose calculated based on which hand is used for the hand-to-face contact
+        
         if(hand[i]=="R"){
-          
+        
         dose[i]<-dose[i-1]+TE.HF[a]*S.F[a]*A.hand[a]*handR[i-1]*exp(-inactiv.hands[a])
         handR[i]<-(1-TE.HF[a]*S.F[a])*handR[i-1]*exp(-inactiv.hands[a])
         handL[i]<-handL[i-1]*exp(-inactiv.hands[a])
@@ -134,6 +168,8 @@ for(a in 1:iter){
     
     #framecheck<-data.frame(handR=handR,handL=handL,fome1=fome1,event=event,hand=hand)
     #View(framecheck)
+    
+    #saving outputs
     
     if (j==1){
       dosetotal=dose
@@ -189,6 +225,8 @@ for(a in 1:iter){
                     timeall=timeall,a.save=a.save,j.save=j.save,dose=dose,eventtotal=eventtotal)
   param<-data.frame(Tehandsurf=Tehandsurf,Tesurfhand=Tesurfhand,totalhand=totalhand,
                     SAfome1=SAfome1,SAfome2=SAfome2,kfome=kfome,khand=khand)
+  
+  #saving overall larger frame for later plotting
   
   if(a==1){
     framesave<-frame
