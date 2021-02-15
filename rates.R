0e0760afbc75e06e7d5eef911a32bda8e2ccfaf4
awilson12 <apfeifer@email.arizona.edu>
2021-02-15 12:57:56 -0700

    fixing timestep error

--- rates.R
+++ rates.R
@@ -14,45 +14,45 @@ non.fome.specific.2<-0.5*S.H[a]*TE.HS[a]
 
 if(j==1|j==2){
   
-  #for Markov models 1 and 2 (symmetrical fome contacts)
+  #for Markov models 1 and 2 (symmetrical fome contacts) (convert to per time step)
   
-  lambda.1.3<-(1/SA.fome.1[a])*non.fome.specific*fome1.markov12
-  lambda.2.3<-(1/SA.fome.2[a])*non.fome.specific*fome2.markov12
+  lambda.1.3<-(1/SA.fome.1[a])*non.fome.specific*fome1.markov12*timestep
+  lambda.2.3<-(1/SA.fome.2[a])*non.fome.specific*fome2.markov12*timestep
   
-  lambda.3.1<-non.fome.specific.2*fome1.markov12
-  lambda.3.2<-non.fome.specific.2*fome2.markov12
+  lambda.3.1<-non.fome.specific.2*fome1.markov12*timestep
+  lambda.3.2<-non.fome.specific.2*fome2.markov12*timestep
   
 }else{
   
   #for Markov models 3 and 4 (asymmetrical fome contacts)
   
-  lambda.1.3<-(1/SA.fome.1[a])*non.fome.specific*fome1.markov34
-  lambda.2.3<-(1/SA.fome.2[a])*non.fome.specific*fome2.markov34
+  lambda.1.3<-(1/SA.fome.1[a])*non.fome.specific*fome1.markov34*timestep
+  lambda.2.3<-(1/SA.fome.2[a])*non.fome.specific*fome2.markov34*timestep
   
-  lambda.3.1<-non.fome.specific.2*fome1.markov34
-  lambda.3.2<-non.fome.specific.2*fome2.markov34
+  lambda.3.1<-non.fome.specific.2*fome1.markov34*timestep
+  lambda.3.2<-non.fome.specific.2*fome2.markov34*timestep
   
 }
 
 #inactivation rates for fomites 1 and 2
-lambda.1.4<-inactiv.fome[a]
-lambda.2.4<-inactiv.fome[a]
+lambda.1.4<-inactiv.fome[a]*timestep
+lambda.2.4<-inactiv.fome[a]*timestep
 
 #loss due to hand washing
 lambda.3.4.b<-(1/10^handsan[a])
 
 if(j==1 | j==3){
   #hand hygiene not included in loss
-  lambda.3.4<-inactiv.hands[a]
+  lambda.3.4<-inactiv.hands[a]*timestep
 }else{
   #hand hygiene included in loss
   #loss due to hand hygiene
-  handhygieneloss<-(1/10^handsan[a])*handsan.markov
-  lambda.3.4<-(1/10^handsan[a])*handsan.markov+inactiv.hands[a]
+  handhygieneloss<-(1/10^handsan[a])*handsan.markov*timestep
+  lambda.3.4<-(1/10^handsan[a])*handsan.markov+inactiv.hands[a]*timestep
 }
 
 #loss from hands due to dose moment (hand-to-mouth contact)
-lambda.3.5<-0.5*S.F[a]*TE.HF[a]*H.face
+lambda.3.5<-0.5*S.F[a]*TE.HF[a]*H.face*timestep
 
 #timing of hand hygiene moments
 handsan.markov.moments<-c(5,10,15,20) #same time as hand hygiene moments in discrete event model
