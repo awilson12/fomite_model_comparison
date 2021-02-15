6e2070c0214dc9e63985727234366319c6ae9ead
awilson12 <apfeifer@email.arizona.edu>
2021-02-15 11:14:53 -0700

    correcting issue with inactivation state

--- Markov_models.R
+++ Markov_models.R
@@ -34,7 +34,7 @@ for (a in 1:iter){
           sim.mat[1,i]<-sim.mat[1,i-1]-sim.mat[1,i-1]*sum(P.1.b.total[2:4])
           sim.mat[2,i]<-sim.mat[2,i-1]-sim.mat[2,i-1]*sum(P.2.b.total[2:4])
           sim.mat[3,i]<-sim.mat[3,i-1]-sim.mat[3,i-1]*P.3.b.total[4]
-          sim.mat[4,i]<-sim.mat[1,i-1]*P.1.b.total[4]+sim.mat[2,i-1]*P.2.b.total[4]+sim.mat[3,i-1]*P.3.b.total[4]
+          sim.mat[4,i]<-sim.mat[4,i-1]+sim.mat[1,i-1]*P.1.b.total[4]+sim.mat[2,i-1]*P.2.b.total[4]+sim.mat[3,i-1]*P.3.b.total[4]
           sim.mat[5,i]<-sim.mat[5,i-1]
           
         
@@ -43,16 +43,16 @@ for (a in 1:iter){
           #other than what is lost due to inactivation
           sim.mat[1,i]<-sim.mat[1,i-1]-sim.mat[1,i-1]*sum(P.1.total[2:4])+sim.mat[3,i-1]*P.3.total[1]
           sim.mat[2,i]<-sim.mat[2,i-1]-sim.mat[2,i-1]*sum(P.2.total[1],P.2.total[3:4])+sim.mat[3,i-1]*P.3.total[2]
-          sim.mat[3,i]<-sim.mat[3,i-1]-sim.mat[3,i-1]*sum(P.3.total[1:2],P.3.total[4])+sim.mat[1,i-1]*P.1.total[3]+sim.mat[2,i-1]*P.2.total[3]
-          sim.mat[4,i]<-sim.mat[1,i-1]*P.1.total[4]+sim.mat[2,i-1]*P.2.total[4]+sim.mat[3,i-1]*P.3.total[4]
+          sim.mat[3,i]<-sim.mat[3,i-1]-sim.mat[3,i-1]*sum(P.3.total[1:2],P.3.total[4:5])+sim.mat[1,i-1]*P.1.total[3]+sim.mat[2,i-1]*P.2.total[3]
+          sim.mat[4,i]<-sim.mat[4,i-1]+sim.mat[1,i-1]*P.1.total[4]+sim.mat[2,i-1]*P.2.total[4]+sim.mat[3,i-1]*P.3.total[4]
           sim.mat[5,i]<-sim.mat[5,i-1]+sim.mat[3,i-1]*P.3.total[5]
         }
       }else{
         #if not models 1 and 3, then losses due to inactivation and hand washing are lumped
         sim.mat[1,i]<-sim.mat[1,i-1]-sim.mat[1,i-1]*sum(P.1.total[2:4])+sim.mat[3,i-1]*P.3.total[1]
         sim.mat[2,i]<-sim.mat[2,i-1]-sim.mat[2,i-1]*sum(P.2.total[1],P.2.total[3:4])+sim.mat[3,i-1]*P.3.total[2]
-        sim.mat[3,i]<-sim.mat[3,i-1]-sim.mat[3,i-1]*sum(P.3.total[1:2],P.3.total[4])+sim.mat[1,i-1]*P.1.total[3]+sim.mat[2,i-1]*P.2.total[3]
-        sim.mat[4,i]<-sim.mat[1,i-1]*P.1.total[4]+sim.mat[2,i-1]*P.2.total[4]+sim.mat[3,i-1]*P.3.total[4]
+        sim.mat[3,i]<-sim.mat[3,i-1]-sim.mat[3,i-1]*sum(P.3.total[1:2],P.3.total[4:5])+sim.mat[1,i-1]*P.1.total[3]+sim.mat[2,i-1]*P.2.total[3]
+        sim.mat[4,i]<-sim.mat[4,i-1]+sim.mat[1,i-1]*P.1.total[4]+sim.mat[2,i-1]*P.2.total[4]+sim.mat[3,i-1]*P.3.total[4]
         sim.mat[5,i]<-sim.mat[5,i-1]+sim.mat[3,i-1]*P.3.total[5]
       }
     
@@ -61,11 +61,11 @@ for (a in 1:iter){
     #saving states, iteration number, model number, and doses
     
     if (j==1){
-      frame<-data.frame(state1=sim.mat[1,],state2=sim.mat[2,],state3=sim.mat[3,],state4=sim.mat[4,],dose=sim.mat[5,],
+      frame<-data.frame(state1=sim.mat[1,]/SA.fome.1[a],state2=sim.mat[2,]/SA.fome.2[a],state3=sim.mat[3,]/(A.hand[a]*2),state4=sim.mat[4,],dose=sim.mat[5,],
                         time=c(1:(duration/timestep)),a=a,j=j)
       max.dose.markov=data.frame(maxdose=max(sim.mat[5,]),a=a,j=j)
     }else{
-      frametemp<-data.frame(state1=sim.mat[1,],state2=sim.mat[2,],state3=sim.mat[3,],state4=sim.mat[4,],dose=sim.mat[5,],
+      frametemp<-data.frame(state1=sim.mat[1,]/SA.fome.1[a],state2=sim.mat[2,]/SA.fome.2[a],state3=sim.mat[3,]/(A.hand[a]*2),state4=sim.mat[4,],dose=sim.mat[5,],
                             time=c(1:(duration/timestep)),a=a,j=j)
       max.dose.markov.temp=data.frame(maxdose=max(sim.mat[5,]),a=a,j=j)
       frame<-rbind(frame,frametemp)
